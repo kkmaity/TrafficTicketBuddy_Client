75597dc26900908c4d56cd4e6c4e49e1113ae7ee
Chayan <chayan.chakraborty@clematistech.com>
2018-05-25 02:48:49 +0530

    update

--- app/src/main/java/com/trafficticketbuddy/client/restservice/RestInterface.java
+++ app/src/main/java/com/trafficticketbuddy/client/restservice/RestInterface.java
@@ -8,6 +8,7 @@ import com.trafficticketbuddy.client.utils.Constant;
 
 import java.util.Map;
 
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -15,6 +16,7 @@ import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.PartMap;
 
 public interface RestInterface {
@@ -32,7 +34,7 @@ public interface RestInterface {
 
     @Multipart
     @POST("api/v1/user/updateClientProfile")
-    Call<LoginMain> editprofile(@PartMap Map<String, RequestBody> map);
+    Call<LoginMain> editprofile(@PartMap Map<String, RequestBody> map, @Part MultipartBody.Part profile_image, @Part MultipartBody.Part license_image);
 
     @POST("api/v1/user/country")
     Call<CountryMain> getCountry();
