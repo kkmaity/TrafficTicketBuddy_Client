ffe1e4dd03d519368d492c759781e2e6f6eff2b6
yaju1234 <yaju.rcc@gmail.com>
2018-07-18 10:40:49 +0530

    live url

--- app/src/main/java/com/trafficticketbuddy/client/utils/Constant.java
+++ app/src/main/java/com/trafficticketbuddy/client/utils/Constant.java
@@ -10,7 +10,8 @@ import java.util.Locale;
  */
 
 public class Constant {
-    public static  String BASE_URL="http://13.58.150.208/buddy/";
+    public static  String BASE_URL="http://www.trafficticketbuddy.ca/buddy/";
+  //  public static  String BASE_URL="http://13.58.150.208/buddy/";
    // public static  String BASE_URL=getBaseURL();
     public static  String API_KEY="0a2b8d7f9243305f2a4700e1870f673a";
     public static String USER_TYPE="CLIENT";
