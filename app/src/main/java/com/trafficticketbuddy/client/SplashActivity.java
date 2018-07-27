fa5e081839bb029e701443171035439a9ae06068
yaju1234 <yaju.rcc@gmail.com>
2018-07-28 00:55:10 +0530

    last commit

--- app/src/main/java/com/trafficticketbuddy/client/SplashActivity.java
+++ app/src/main/java/com/trafficticketbuddy/client/SplashActivity.java
@@ -1,15 +1,23 @@
 package com.trafficticketbuddy.client;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.gson.Gson;
 import com.trafficticketbuddy.client.model.login.Response;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class SplashActivity extends BaseActivity {
     private String deviceToken;
@@ -26,7 +34,7 @@ public class SplashActivity extends BaseActivity {
 
 
 
-      /*  PackageInfo info;
+        PackageInfo info;
         try {
             info = getPackageManager().getPackageInfo("com.trafficticketbuddy.client", PackageManager.GET_SIGNATURES);
             for (Signature signature : info.signatures) {
@@ -35,7 +43,7 @@ public class SplashActivity extends BaseActivity {
                 md.update(signature.toByteArray());
                 String something = new String(Base64.encode(md.digest(), 0));
                 //String something = new String(Base64.encodeBytes(md.digest()));
-                Log.e("hash key", something);
+                Log.e("!!!!!!!!hash key", something);
             }
         } catch (PackageManager.NameNotFoundException e1) {
             Log.e("name not found", e1.toString());
@@ -43,7 +51,7 @@ public class SplashActivity extends BaseActivity {
             Log.e("no such an algorithm", e.toString());
         } catch (Exception e) {
             Log.e("exception", e.toString());
-        }*/
+        }
 
 
         callNewScreen();
@@ -100,4 +108,5 @@ public class SplashActivity extends BaseActivity {
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         preference.setDeviceToken(refreshedToken);
     }
+
 }
